a2aabccb901c6103dd83e63df396f3028029a201
FannarG <118724990+FannarG@users.noreply.github.com>
2023-05-03 13:23:09 +0000

    dót fyrir urls

--- pizza_lair/pizza_lair/urls.py
+++ pizza_lair/pizza_lair/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('menu.urls')),
-    path('menu/', include('menu.urls'))
+    path('menu/', include('menu.urls')),
+    path('myprofile', include('Myprofile.urls'))
 ]
