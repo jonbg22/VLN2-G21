45658efd554141b7f3f6d28f861f95ba270770a5
jonbg22 <jonbg22@ru.is>
2023-05-04 14:46:51 +0000

    drinks and sides work

--- pizza_lair/menu/views.py
+++ pizza_lair/menu/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render, get_object_or_404
 from menu.models import Pizza,Side,Drink
 
+
 def index(request):
     return render(request, 'menu/pizzas.html', {
         'pizzas': Pizza.objects.select_related("prod")
     })
 
-def get_pizza_by_id(request,id):
+
+def get_pizza_by_id(request, id):
     return render(request, 'menu/pizza_details.html', {
-        'pizza': get_object_or_404(Pizza, pk=id)
+        'pizza': get_object_or_404(Pizza, prod=id)
     })
 
 
@@ -18,7 +20,19 @@ def sides(request):
     })
 
 
+def get_side_by_id(request, id):
+    return render(request, 'menu/side_details.html', {
+        'side': get_object_or_404(Side, prod=id)
+    })
+
+
 def drinks(request):
     return render(request, 'menu/drinks.html', {
         'drinks': Drink.objects.select_related("prod")
+    })
+
+
+def get_drink_by_id(request, id):
+    return render(request, 'menu/drink_details.html', {
+        'drink': get_object_or_404(Drink, prod=id)
     })
