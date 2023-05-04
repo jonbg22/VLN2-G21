20eb461abaa7bd626544149f20ed1fbc653fa217
FannarG <118724990+FannarG@users.noreply.github.com>
2023-05-04 14:12:11 +0000

    myprofile

--- pizza_lair/Myprofile/urls.py
+++ pizza_lair/Myprofile/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 from . import views
 urlpatterns = [
-    # http://localhost:8000/myProfile
+    # http://localhost:8000/profile
     path('', views.index, name="index"),
 ]
--- pizza_lair/Myprofile/views.py
+++ pizza_lair/Myprofile/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 
 # Create your views here.
 def index(request):
-    return HttpResponse('<h1>blabla from My profile </h1>')
+    return render(request, 'myProfile/index.html')
