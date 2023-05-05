bba97e8a2f6237f79162adca01f4235d0dbb5920
FannarG <118724990+FannarG@users.noreply.github.com>
2023-05-05 15:18:51 +0000

    Profile

--- pizza_lair/profile/views.py
+++ pizza_lair/profile/views.py
@@ -5,3 +5,7 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'profile/index.html')
+
+def register(request):
+    return render(request, 'profile/register.html')
+
