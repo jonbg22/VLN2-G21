39e46394120547d8fd3f951245879a674cafb7e3
jongudni <jongud22@ru.is>
2023-05-05 10:49:15 +0000

    allskonar stuff í offers, mega flott

--- pizza_lair/offers/views.py
+++ pizza_lair/offers/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from offers.models import Offer
+from menu.models import Pizza, Drink, Side
 # Create your views here.
 def index(request):
     return render(request, 'offers/offers.html', {
@@ -7,7 +8,29 @@ def index(request):
     })
 
 def get_offer_by_id(request, id):
+    offer = get_object_or_404(Offer, pk=id)
+    pizza_amount = []
+    for i in range(offer.amountPizza):
+        i = i+1
+        pizza_amount.append(i)
+    sides_amount = []
+    for i in range(offer.amountSides):
+        i = i+1
+        sides_amount.append(i)
+    drinks_amount = []
+    for i in range(offer.amountDrinks):
+        i = i+1
+        drinks_amount.append(i)
     return render(request, 'offers/offer_details.html', {
-        'offer': get_object_or_404(Offer, pk=id)
+        'offer': offer,
+        'pizza_amount': pizza_amount,
+        'sides_amount': sides_amount,
+        'drinks_amount': drinks_amount,
+        "pizzas": Pizza.objects.all(),
+        "drinks": Drink.objects.all(),
+        "sides": Side.objects.all()
+
     })
+
+
 
