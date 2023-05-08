97883f99cbcd32f4c0b9653f9c22b1a2bad761c7
FannarG <118724990+FannarG@users.noreply.github.com>
2023-05-08 14:18:08 +0000

    login

--- pizza_lair/profile/urls.py
+++ pizza_lair/profile/urls.py
@@ -1,8 +1,10 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.contrib.auth.views import LoginView, LogoutView
+from django.urls import path
 from . import views
+
 urlpatterns = [
     # http://localhost:8000/profile
-    path('', views.index, name="index"),
-    path('register', views.register, name='register')
+    path('register', views.register, name='register'),
+    path('login', LoginView.as_view(template_name='profile/login.html'), name='login'),
+    path('logout', LogoutView.as_view(next_page='login'), name='logout')
 ]
