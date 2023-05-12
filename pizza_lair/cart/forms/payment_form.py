a27db28c7ccc11d81732b21b0e2f2283e22393bc
FannarG <118724990+FannarG@users.noreply.github.com>
2023-05-12 10:19:34 +0000

    credit car validation

--- pizza_lair/cart/forms/payment_form.py
+++ pizza_lair/cart/forms/payment_form.py
@@ -1,8 +1,9 @@
 from django import forms
 from creditcards.forms import CardNumberField, CardExpiryField, SecurityCodeField
+from django.core.validators import RegexValidator
 
 class PaymentForm(forms.Form):
     name_on_card = forms.CharField(max_length=50, required=True)
-    card_number = forms.IntegerField(required=True)
+    card_number = forms.CharField(required=True, min_length=16, max_length=16, validators=[RegexValidator("^(?:-(?:[1-9](?:\\d{0,2}(?:,\\d{3})+|\\d*))|(?:0|(?:[1-9](?:\\d{0,2}(?:,\\d{3})+|\\d*))))(?:.\\d+|)$")])
     expiry_date = CardExpiryField(required=True)
     cvc = SecurityCodeField(required=True)
