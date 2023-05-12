c7a504de8e705f079459055f11a6a4cb2e779ff3
jonbg22 <jonbg22@ru.is>
2023-05-12 12:31:02 +0000

    Finished implementing pizza of the day

--- pizza_lair/home/views.py
+++ pizza_lair/home/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 import datetime
+from offers.models import get_todays_pizza
+from menu.models import Pizza
+
 
 def index(request):
+    today = datetime.datetime.today()
+    day_int = today.weekday()
     return render(request, 'index.html', {
-        "weekday": datetime.datetime.today().strftime('%A')
+        "weekday": today.strftime('%A'),
+        'pizza': get_todays_pizza()
     })
